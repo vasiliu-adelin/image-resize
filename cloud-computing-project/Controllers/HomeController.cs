67404bee5eda66081faebbad3e7911812fde3d65
adelin <vasiliu.gheorghe.adelin@gmail.com>
2024-05-14 01:02:36 +0300

    migrated to .net core 6.0

---
+++ cloud-computing-project/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using cloud_computing_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace cloud_computing_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+    }
+}
---
+++ cloud-computing-project/Helpers/StorageHelper.cs
@@ -0,0 +1,63 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Azure.Storage;
+using cloud_computing_project.Models;
+
+namespace cloud_computing_project.Helpers
+{
+    public static class StorageHelper
+    {
+
+        public static bool IsImage(IFormFile file)
+        {
+            if (file.ContentType.Contains("image"))
+            {
+                return true;
+            }
+
+            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg" };
+
+            return formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static async Task<bool> UploadFileToStorage(Stream fileStream, string fileName,
+                                                            AzureStorageConfig _storageConfig)
+        {
+            Uri blobUri = new Uri("https://" +
+                                  _storageConfig.AccountName +
+                                  ".blob.core.windows.net/" +
+                                  _storageConfig.ImageContainer +
+                                  "/" + fileName);
+
+            StorageSharedKeyCredential storageCredentials =
+                new StorageSharedKeyCredential(_storageConfig.AccountName, _storageConfig.AccountKey);
+
+            BlobClient blobClient = new BlobClient(blobUri, storageCredentials);
+
+            await blobClient.UploadAsync(fileStream);
+
+            return await Task.FromResult(true);
+        }
+
+        public static async Task<List<string>> GetThumbNailUrls(AzureStorageConfig _storageConfig)
+        {
+            List<string> thumbnailUrls = new List<string>();
+
+            Uri accountUri = new Uri("https://" + _storageConfig.AccountName + ".blob.core.windows.net/");
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(accountUri);
+
+            BlobContainerClient container = blobServiceClient.GetBlobContainerClient(_storageConfig.ThumbnailContainer);
+
+            if (container.Exists())
+            {
+                foreach (BlobItem blobItem in container.GetBlobs())
+                {
+                    thumbnailUrls.Add(container.Uri + "/" + blobItem.Name);
+                }
+            }
+
+            return await Task.FromResult(thumbnailUrls);
+        }
+    }
+}
